e89e183978915209428778205c7dc33830e77447
Islenna <dockral@gmail.com>
2023-10-13 22:58:13 -0700

    recipe card and sass

--- frontend/src/app/recipe-display/recipe-display.component.ts
+++ frontend/src/app/recipe-display/recipe-display.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-recipe-display',
   templateUrl: './recipe-display.component.html',
-  styleUrls: ['./recipe-display.component.sass']
+  styleUrls: ['./recipe-display.component.sass'],
+  
 })
+
 export class RecipeDisplayComponent {
+  
+  toggleCompletion(event: any) {
+    const target = event.target.closest('li');
+    if (target) {
+        target.classList.toggle('completed');
+    }
+}
 
 }

fbb918bb6e6828a0b381e212bc70ce4cefeb8af6
Islenna <dockral@gmail.com>
2023-10-16 11:41:01 -0700

    Box setup

--- frontend/src/app/dash/dash.component.ts
+++ frontend/src/app/dash/dash.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
-  selector: 'app-dash',
+  selector: 'app-chest',
   templateUrl: './dash.component.html',
-  styleUrls: ['./dash.component.sass']
+  styleUrls: ['./dash.component.sass'],
+  animations: [
+    trigger('swingUp', [
+      state('closed', style({
+        transform: 'rotateX(0)'
+      })),
+      state('open', style({
+        transform: 'rotateX(90deg)'
+      })),
+      transition('closed <=> open', [
+        animate('2s')
+      ]),
+    ]),
+    trigger('revealContents', [
+      state('hidden', style({
+        opacity: 0,
+      })),
+      state('visible', style({
+        opacity: 1,
+      })),
+      transition('hidden <=> visible', [
+        animate('2s')
+      ]),
+    ])
+  ]
 })
 export class DashComponent {
+  isLidOpen = false;
+  areContentsVisible = false;
 
+  toggleChest(): void {
+    this.isLidOpen = !this.isLidOpen;
+    this.areContentsVisible = !this.areContentsVisible;
+  }
 }
--- frontend/src/app/recipe.service.ts
+++ frontend/src/app/recipe.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RecipeService {
+  // URL to your backend API endpoint that gets all recipes.
+  private apiUrl = 'http://localhost:3000/api/recipes'; // Change with your actual URL
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAllRecipes() {
+    return this.http.get(this.apiUrl);
+  }
 }
